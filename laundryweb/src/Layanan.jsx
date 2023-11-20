a2e3ea9a6271009e6cb39391a818fbd108e39004
PutriPrema <putriprema14@gmail.com>
2023-11-20 14:43:18 +0800

    layanan edit by putri

--- laundryweb/src/Layanan.jsx
+++ laundryweb/src/Layanan.jsx
@@ -1,9 +1,9 @@
-// eslint-disable-next-line no-unused-vars
+/* eslint-disable no-unused-vars */
 import React from 'react'
 
 const Layanan = () => {
   return (
-    <div>ini Layanan</div>
+    <div>Layanan</div>
   )
 }
 
